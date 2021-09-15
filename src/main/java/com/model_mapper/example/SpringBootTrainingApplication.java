18cd5143dbba6889a1f8958caf87924635e41298
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-15 22:52:14 +0700

    Update SpringBootTrainingApplication.java

--- src/main/java/com/model_mapper/example/SpringBootTrainingApplication.java
+++ src/main/java/com/model_mapper/example/SpringBootTrainingApplication.java
@@ -3,13 +3,11 @@ package com.model_mapper.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Date;
 
 @SpringBootApplication
 public class SpringBootTrainingApplication {
 
     public static void main(String[] args) {
-        System.out.println(new Date());
         SpringApplication.run(SpringBootTrainingApplication.class, args);
     }
 
