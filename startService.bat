292d5171907c2e8f3d1f8a024fc8c74b0163e2a2
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-15 21:45:22 +0700

    Update startService.bat

--- startService.bat
+++ startService.bat
@@ -1,5 +1,5 @@
 echo Current Dir "%CD%"
 xcopy /s /Y "C:\Users\Andika Mulyawan\.jenkins\workspace\UtilsModelMapper" E:\GIT\PUBLISH\UtilsModelMapper
 cd /d E:
-cd "E:\GIT\PUBLISH\UtilsModelMapper"
+cd "E:\GIT\PUBLISH\UtilsModelMapper\target"
 java -jar -Xms1024m -Xmx1024m Model_Mapper-0.0.1-SNAPSHOT.jar
