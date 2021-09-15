df3a1c1caad512a244f3393ff984cf871aec21a9
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-15 20:29:53 +0700

    update bat file

--- src/main/java/com/modelMapper/example/SpringBootTrainingApplication.java
+++ src/main/java/com/modelMapper/example/SpringBootTrainingApplication.java
@@ -3,10 +3,13 @@ package com.modelMapper.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class SpringBootTrainingApplication {
 
 	public static void main(String[] args) {
+		System.out.println(new Date());
 		SpringApplication.run(SpringBootTrainingApplication.class, args);
 	}
 
