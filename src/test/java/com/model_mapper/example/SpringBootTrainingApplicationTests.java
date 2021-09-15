4c3299f814e2303f39b900bf85f5544f4895d941
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-15 23:21:29 +0700

    Create SpringBootTrainingApplicationTests.java

---
+++ src/test/java/com/model_mapper/example/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.model_mapper.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+    @Test
+    public void main() {
+        SpringBootTrainingApplication.main(new String[] {});
+    }
+}
