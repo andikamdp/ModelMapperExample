0bb3ca25abafe60d9b1ac9b440c37565a92cc17e
andikamdp <31431078+andikamdp@users.noreply.github.com>
2021-09-15 20:56:03 +0700

    Update startService.bat

--- startService.bat
+++ startService.bat
@@ -1,15 +1,16 @@
 echo Current Dir "%CD%"
 TIMEOUT /T 10
-xcopy /s /Y "C:\Users\Andika Mulyawan\.jenkins\workspace\UtilsModelMapper" E:\GIT\PUBLISH\UtilsModelMapper
-TIMEOUT /T 10
-cd /d E:
-TIMEOUT /T 10
-cd "E:\GIT\PUBLISH\UtilsModelMapper"
-TIMEOUT /T 10
 call mvn clean package
 TIMEOUT /T 10
 call mvn test
 TIMEOUT /T 10
 call mvn sonar:sonar -Dsonar.projectKey=UtilsModelMapper -Dsonar.host.url=http://localhost:9000 -Dsonar.login=eca13c9d76f904428452ca07b7e6a120cc391299
 TIMEOUT /T 10
+xcopy /s /Y "C:\Users\Andika Mulyawan\.jenkins\workspace\UtilsModelMapper" E:\GIT\PUBLISH\UtilsModelMapper
+TIMEOUT /T 10
+cd /d E:
+TIMEOUT /T 10
+cd "E:\GIT\PUBLISH\UtilsModelMapper"
+TIMEOUT /T 10
+
 java -jar -Xms1024m -Xmx1024m Model_Mapper-0.0.1-SNAPSHOT.jar
